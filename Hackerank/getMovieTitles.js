b7d0eac821ea61c2cad29bb8db95e1a472de317f
Edmond Atto <edmond091@gmail.com>
2020-05-18 23:36:19 -0400

    Add JSDocs and refactor for readability

--- Hackerank/getMovieTitles.js
+++ Hackerank/getMovieTitles.js
@@ -7,6 +7,12 @@
 
 const https = require('https');
 
+/**
+ * Returns the number of number of pages of results fetched from a URL
+ * @param {string} url - The URL from which the results are to be fetched
+ * @param {function} callback - The callback to be executed when the function returns
+ * @return {function}
+ */
 function getTotalPageCount(url, callback) {
   https.get(url, response => {
     let data = '';
@@ -21,7 +27,13 @@ function getTotalPageCount(url, callback) {
   })
 }
 
-function getAggregatedResults(resultPagesUrls, callback) {
+/**
+ * Returns the aggregated results from pages of results fetched from a URL
+ * @param {string[]} resultPagesUrls - An array of URLs from which the titles are to be fetched and aggregated
+ * @param {function} callback - The callback to be executed when the function returns
+ * @return {function}
+ */
+function getAggregatedTitles(resultPagesUrls, callback) {
   let titles = [];
   let pagesRequested = 1;
 
@@ -39,31 +51,39 @@ function getAggregatedResults(resultPagesUrls, callback) {
         if (pagesRequested === resultPagesUrls.length) {
           return callback(null, titles);
         }
+
         pagesRequested++;
       })
-    }).on('error', err => {
-      return callback(err, null);
+    }).on('error', error => {
+      return callback(error, null);
     })
   })
 }
 
+/**
+ * Returns all the movie titles fetched from an API endpoint when provided a search term
+ * @param {string} substr - The search term to be matched against movie titles by the API
+ * @return
+ */
 function getMovieTitles(substr) {
-  if (substr === '') return null
+  if (typeof substr !== 'string' || !substr) return null;
 
   const baseURL = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=';
   const url = `${baseURL}${substr}`;
 
-  getTotalPageCount(url, (err, totalPages) => {
-    if (err) {
-      console.log('Error:', err);
+  getTotalPageCount(url, (error, totalPages) => {
+    if (error) {
+      console.log('Error:', error);
     } else {
       const resultPagesUrls = [];
+
       for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
         resultPagesUrls.push(`${url}&page=${pageNumber}`);
       }
-      getAggregatedResults(resultPagesUrls, (err, titles) => {
-        if (err) {
-          console.log('Error:', err);
+
+      getAggregatedTitles(resultPagesUrls, (error, titles) => {
+        if (error) {
+          console.log('Error:', error);
         } else {
           console.log(titles.sort());
         }
