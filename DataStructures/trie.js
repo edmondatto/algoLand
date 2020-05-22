049d278855508bfbf3b74c4c935d6b3e106b292f
Edmond Atto <edmond091@gmail.com>
2020-05-21 20:40:25 -0400

    Add JSDocs for self-documentation

--- DataStructures/trie.js
+++ DataStructures/trie.js
@@ -1,3 +1,4 @@
+/* A Class that represents a Node in a Trie */
 class Node {
   constructor() {
     this.children = []
@@ -5,18 +6,38 @@ class Node {
     this.isWord = false
   }
 
+  /**
+   * Return the index of an alphabetic character
+   * @param {string} char - A string character
+   * @return {number}
+   */
   getCharIndex(char) {
     return char.charCodeAt() - 'a'.charCodeAt()
   }
 
+  /**
+   * Return the node corresponding to an alphabetic character
+   * @param {string} char - A string character
+   * @return {Node}
+   */
   getNode(char) {
     return this.children[this.getCharIndex(char)]
   }
 
+  /**
+   * Return the node corresponding to an alphabetic character
+   * @param {string} char - A string character
+   * @param {Node} node - The node to be set as a child to the current node
+   */
   setNode(char, node) {
     this.children[this.getCharIndex(char)] = node;
   }
 
+  /**
+   * Add a word to the trie
+   * @param {string} word - The word to be added
+   * @param {number} index - The starting index from which to add the word
+   */
   addWord(word, index = 0) {
     this.size++
     if (index === word.length) {
@@ -33,10 +54,19 @@ class Node {
     child.addWord(word, index + 1)
   }
 
+  /**
+   * Add multiple words to the trie
+   * @param {string[]} words - An array of words to be added
+   */
   addWords (words) {
     words.forEach(word => this.addWord(word))
   }
 
+  /**
+   * Find how many words match a search term
+   * @param {string} word - A word to be used as a search term
+   * @param {number} index - The starting index from which to search for the word
+   */
   findCount (word, index = 0) {
     if (index === word.length) return this.size
 
