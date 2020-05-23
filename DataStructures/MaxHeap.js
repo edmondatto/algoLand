365be293538fef6ce94d33357132ea99ad4b16dd
Edmond Atto <edmond091@gmail.com>
2020-05-23 15:07:44 -0400

    Add MaxHeap to data structures

---
+++ DataStructures/MaxHeap.js
@@ -0,0 +1,164 @@
+/* A Class that represents a Max Heap */
+class MaxHeap {
+  constructor() {
+    this.items = []
+  }
+
+  /**
+   * Returns the heap's size
+   * @return {number}
+   */
+  size() {
+    return this.items.length
+  }
+
+  /**
+   * Gets a an Item's left child's index
+   * @param {number} index - The parent's index
+   * @return {number}
+   */
+  getLeftChildIndex(index) {
+    return (index * 2) + 1
+  }
+
+  /**
+   * Gets a an Item's left child
+   * @param {number} index - The parent's index
+   * @return {number}
+   */
+  getLeftChild(index) {
+    return this.items[this.getLeftChildIndex(index)] || null
+  }
+
+  /**
+   * Checks if an Item has a left child
+   * @param {number} index - The parent's index
+   * @return {boolean}
+   */
+  hasLeftChild(index) {
+    return !!this.getLeftChild(index)
+  }
+
+  /**
+   * Gets a an Item's right child's index
+   * @param {number} index - The parent's index
+   * @return {number}
+   */
+  getRightChildIndex(index) {
+    return (index * 2) + 2
+  }
+
+  /**
+   * Gets a an Item's right child
+   * @param {number} index - The parent's index
+   * @return {number}
+   */
+  getRightChild(index) {
+    return this.items[this.getRightChildIndex(index)]
+  }
+
+  /**
+   * Checks if an Item has a right child
+   * @param {number} index - The parent's index
+   * @return {boolean}
+   */
+  hasRightChild(index) {
+    return !!this.getRightChild(index)
+  }
+
+  /**
+   * Gets a an Item's parent's index
+   * @param {number} index - The child's index
+   * @return {number}
+   */
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2)
+  }
+
+  /**
+   * Gets a an Item's parent
+   * @param {number} index - The child's index
+   * @return {number}
+   */
+  getParent(index) {
+    return this.items[this.getParentIndex(index)]
+  }
+
+  /**
+   * Checks if an Item has a parent
+   * @param {number} index - The child's index
+   * @return {boolean}
+   */
+  hasParent(index) {
+    return !!this.getParent(index)
+  }
+
+  /**
+   * Add an Item to the heap
+   * @param {number} item - The item to be added
+   */
+  add(item) {
+    this.items.push(item)
+    this.heapifyUp()
+  }
+
+  /**
+   * Returns the item at the top the the heap
+   */
+  peek() {
+    return this.items[0]
+  }
+
+  /**
+   * Returns the maximum value and removes it from the the heap
+   * @return {number}
+   */
+  poll() {
+    const item = this.items[0] || null
+    this.items[0] = this.items.pop()
+    this.heapifyDown()
+    return item
+  }
+
+  /**
+   * Swaps a parent with its child
+   * @param {number} childIndex - The child's index
+   * @param {number} parentIndex - The parent's index
+   */
+  swap(childIndex, parentIndex) {
+    const child = this.items[childIndex]
+    this.items[childIndex] = this.items[parentIndex]
+    this.items[parentIndex] = child
+  }
+
+  /**
+   * Alters the heap from the bottom up if necessary, to ensure it follows the rules of a max heap
+   */
+  heapifyUp() {
+    let index = this.items.length - 1
+    while (this.hasParent(index) && this.items[index] > this.getParent(index)) {
+      this.swap(index, this.getParentIndex(index))
+      index = this.getParentIndex(index)
+    }
+  }
+
+  /**
+   * Alters the heap from the top down if necessary, to ensure it follows the rules of a max heap
+   */
+  heapifyDown() {
+    let index = 0
+    while (this.hasLeftChild(index) && (this.items[index] < this.getLeftChild(index) || this.items[index] < this.getRightChild(index))) {
+      let biggerChildIndex = this.getLeftChildIndex(index)
+
+      if (this.hasRightChild(index)) {
+        biggerChildIndex = this.getLeftChild(index) < this.getRightChild(index) ? this.getRightChildIndex(index) : biggerChildIndex
+      }
+      this.swap(biggerChildIndex, index)
+      index = biggerChildIndex
+    }
+  }
+
+  print() {
+    return this.items
+  }
+}
