f790ce694fc4d72f9aea01b9bd43adf65a0f9d44
Edmond Atto <edmond091@gmail.com>
2020-05-31 23:56:10 -0400

    Add documentation to queue made using two stacks

--- DataStructures/QueueUsingTwoStacks.js
+++ DataStructures/QueueUsingTwoStacks.js
@@ -1,11 +1,16 @@
 import Stack from "./Stack";
 
+/* A Class representing a Stack */
 class QueueUsingTwoStacks {
   constructor() {
     this.stackWithNewestItemOnTop = new Stack();
     this.stackWithOldestItemOnTop = new Stack();
   }
 
+  /**
+   * Moves all items from the stack with the oldest items to the one with new items
+   * @return void
+   */
   #shiftItemsToOldest() {
     if(this.stackWithOldestItemOnTop.isEmpty()) {
       while (!this.stackWithNewestItemOnTop.isEmpty()) {
@@ -14,15 +19,28 @@ class QueueUsingTwoStacks {
     }
   }
 
+  /**
+   * Returns the item at the front of the queue
+   * @return {*} - The item at the front of the queue
+   */
   peek() {
     this.#shiftItemsToOldest();
     return this.stackWithOldestItemOnTop.top();
   }
 
+  /**
+   * Adds an item to the back of the queue
+   * @param {*} item - The item to be added to the queue
+   * @return void
+   */
   enqueue(item) {
     this.stackWithNewestItemOnTop.push(item);
   }
 
+  /**
+   * Removes the item at the front of the queue and returns it
+   * @return {*} item - The item to be removed from the queue
+   */
   dequeue() {
     this.#shiftItemsToOldest();
     return this.stackWithOldestItemOnTop.pop();
