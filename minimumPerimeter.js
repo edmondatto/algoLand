42032904479380630ac17068861e96f2276c361e
Edmond Atto <edmond091@gmail.com>
2020-06-03 10:43:49 -0400

    Add minimumPerimeter solution

---
+++ minimumPerimeter.js
@@ -0,0 +1,32 @@
+/**
+ * Returns the minimum perimeter of a rectangle given its area
+ * @params {number} area - The area of the rectangle
+ * @return {number} The computed minimum perimeter
+ */
+function minimumPerimeter(area) {
+  let width = 1
+  let minPerimeter = Number.MAX_SAFE_INTEGER
+
+  while (width < area/2) {
+    const modulo = area % width
+    if (modulo === 0) {
+      const length = area / width
+      const currentPerimeter = calculatePerimeter(length, width)
+      if (minPerimeter > currentPerimeter) {
+        minPerimeter = currentPerimeter
+      }
+    }
+    width++
+  }
+  return minPerimeter
+}
+
+/**
+ * Returns the perimeter of a rectangle given its length and width
+ * @params {number} length - The length of the rectangle
+ * @params {number} width - The width of the rectangle
+ * @return {number} The computed perimeter
+ */
+function calculatePerimeter(length, width) {
+  return 2 * (length + width)
+}
