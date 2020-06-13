cf9fcaa3c4a1752119fcb2278b4774775a436679
Edmond Atto <edmond091@gmail.com>
2020-06-13 00:02:22 -0400

    Improve sumZero solution

--- LeetCode/easy/sumZero.js
+++ LeetCode/easy/sumZero.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number[]}
  */
-function sumZero(n) {
+function sumZero(n) { // O(n) solution
   const resultArray = []
   let currentSum = 0
 
@@ -17,18 +17,15 @@ function sumZero(n) {
   return resultArray
 }
 
-function sumZero2(n) {
+function sumZero2(n) { // O(n/2) solution
   const resultArray = []
 
   for (let i = n; i > Math.ceil(n/2); i--) {
     resultArray.push(i)
+    resultArray.push(-i)
   }
 
   if (n % 2 === 1) resultArray.push(0)
-
-  for (let i = 0; i < Math.floor(n/2); i++) {
-    resultArray.push(resultArray[i] * -1)
-  }
 
   return resultArray
 }
