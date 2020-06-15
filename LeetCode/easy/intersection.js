04179689c71a0031473b179dd037afb08e3381b0
Edmond Atto <edmond091@gmail.com>
2020-06-15 16:12:58 -0400

    Add in-place solution

--- LeetCode/easy/intersection.js
+++ LeetCode/easy/intersection.js
@@ -16,3 +16,29 @@ function intersection(nums1, nums2) {
   }
   return [...result]
 }
+
+function intersection2(nums1, nums2) {
+  nums1.sort(function(a, b) { return a - b})
+  nums2.sort(function(a, b) { return a - b})
+  const longerArray = nums2.length >= nums1.length ? nums2 : nums1
+  const shorterArray = nums2.length < nums1.length ? nums2 : nums1
+  const result = []
+
+  if (shorterArray[0] > longerArray[longerArray.length - 1] ||
+    shorterArray[shorterArray[shorterArray.length - 1] < longerArray[0]]) {
+    return result
+  }
+
+  let i = 0, j = 0;
+  while (i < shorterArray.length) {
+    if (shorterArray[i] === longerArray[j] && result[result.length - 1] !== shorterArray[i]) {
+      result.push(shorterArray[i])
+      j++
+    } else if (shorterArray[i] > longerArray[j]) {
+      j++
+    } else {
+      i++
+    }
+  }
+  return result
+}
