04ae28332e401f1698246869e48ef91e14c4a3c1
Edmond Atto <edmond091@gmail.com>
2020-06-18 00:49:28 -0400

    Add containsDuplicate solution

---
+++ LeetCode/easy/containsDuplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate (nums) {
+  const resultSet = new Set(nums)
+
+  return resultSet.size !== nums.length
+}
+
+function containsDuplicate2(nums) {
+  nums.sort(function(a, b) { return a - b })
+  console.log(nums)
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === nums[i + 1]) return true
+  }
+  return false
+}
