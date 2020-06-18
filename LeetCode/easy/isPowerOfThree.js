2c959c9a73aeadb50f3fc50aae8212804ca502d0
Edmond Atto <edmond091@gmail.com>
2020-06-18 00:34:41 -0400

    Add constant time solution

--- LeetCode/easy/isPowerOfThree.js
+++ LeetCode/easy/isPowerOfThree.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {boolean}
  */
-function isPowerOfThree(n) {
+function isPowerOfThree(n) { // O(logN)
   if (n < 1) return false
 
   while (n % 3 === 0) {
@@ -10,4 +10,9 @@ function isPowerOfThree(n) {
   }
 
   return n === 1
+}
+
+function isPowerOfThree2(n) { // O(1)
+  // 3 ^ 19 is the largest integer value that's a power of 3
+  return n > 0 && Math.pow(3, 19) % 3 === 0
 }
