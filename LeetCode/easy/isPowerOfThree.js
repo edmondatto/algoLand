f4e23926ecf3dff5db0f2a86f1971a98c2f8f2bc
Edmond Atto <edmond091@gmail.com>
2020-06-18 00:07:01 -0400

    Tweak isPowerOfThree solution

--- LeetCode/easy/isPowerOfThree.js
+++ LeetCode/easy/isPowerOfThree.js
@@ -4,12 +4,10 @@
  */
 function isPowerOfThree(n) {
   if (n < 1) return false
-  while (n) {
-    if (n === 1) return true
-    if (n % 3 === 0) {
-      n /= 3
-    } else {
-      return false
-    }
+
+  while (n % 3 === 0) {
+    n /= 3
   }
+
+  return n === 1
 }
