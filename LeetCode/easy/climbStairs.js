6666e66ba9aacdcd38bfb189dfa7c66f4b8fa752
Edmond Atto <edmond091@gmail.com>
2020-06-20 11:06:41 -0400

    Add O(1) space solution

--- LeetCode/easy/climbStairs.js
+++ LeetCode/easy/climbStairs.js
@@ -2,11 +2,25 @@
  * @param {number} n
  * @return {number}
  */
-function climbStairs(n) {
+function climbStairs(n) { // Time Complexity => O(n) Space complexity => O(n)
   let numOfSteps = [0, 1, 2]
   for (let i = 3; i <= n; i++) {
     numOfSteps[i] = numOfSteps[i - 1] + numOfSteps[i - 2]
   }
   return numOfSteps[n ]
 
+}
+
+function climbStairs2(n) { // Time Complexity => O(n) Space complexity => O(1)
+  if (n === 1) return 1
+
+  let first = 1
+  let second = 2
+
+  for (let i = 3; i <= n; i++) {
+    const third = first + second
+    first = second
+    second = third
+  }
+  return second
 }
