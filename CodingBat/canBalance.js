21839aeeffd71a8e85ec2886deb3ff5615865432
Edmond Atto <edmond091@gmail.com>
2020-06-24 15:48:43 -0400

    Add solution variant that returns balanced arrays

--- CodingBat/canBalance.js
+++ CodingBat/canBalance.js
@@ -4,9 +4,9 @@
 // canBalance([1, 1, 1, 2, 1]) → true
 // canBalance([2, 1, 1, 2, 1]) → false
 // canBalance([10, 10]) → true
+// canBalance([1, 2, 3, 4, 5, 2, 1, 1, 1]) → true
 
 function canBalance(nums) {
-
   let i = 0, j = nums.length - 1, rSum = 0, lSum = 0
 
   while (i <= j) {
@@ -19,4 +19,22 @@ function canBalance(nums) {
     }
   }
   return rSum === lSum
+}
+
+// balance([1, 1, 1, 2, 1]) → [ [ 1, 1, 1 ], [ 2, 1 ] ]
+// balance([2, 1, 1, 2, 1]) → [ 2, 1, 1, 2, 1 ]
+// balance([1, 2, 3, 4, 5, 2, 1, 1, 1]) → [ [ 1, 2, 3, 4 ], [ 5, 2, 1, 1, 1 ] ]
+function balance(nums) {
+  let i = 0, j = nums.length - 1, rSum = 0, lSum = 0
+
+  while (i <= j) {
+    if (rSum <= lSum) {
+      rSum += nums[i]
+      i++
+    } else {
+      lSum += nums[j]
+      j--
+    }
+  }
+  return rSum === lSum ? [ nums.slice(0, i), nums.slice(j + 1) ] : nums
 }
