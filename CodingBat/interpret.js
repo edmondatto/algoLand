543a98a1eb9294da8eaa2c1f8b2aa779488151b8
Edmond Atto <edmond091@gmail.com>
2020-06-25 23:20:02 -0400

    Add interpret solution

---
+++ CodingBat/interpret.js
@@ -0,0 +1,36 @@
+// interpret(1, ['+'], [1]) → 2
+// interpret(4, ['-'], [2]) → 2
+// interpret(1, ['+', '*'], [1, 3]) → 6
+
+function interpret(value, commands, args) {
+  const commandsMap = {
+    '+': 'add',
+    '-': 'sub',
+    '*': 'mul',
+    '/': 'div'
+  }
+
+  let i = 0
+
+  while (i < commands.length) {
+    const command = commandsMap[commands[i]]
+    if (!command) return -1
+
+    const arg = args[i]
+    switch (command) {
+      case 'add':
+        value += arg
+        break
+      case 'sub':
+        value -= arg
+        break
+      case 'mul':
+        value *= arg
+        break
+      default:
+        return
+    }
+    i++
+  }
+  return value
+}
