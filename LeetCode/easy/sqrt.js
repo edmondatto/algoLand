c51755bebadefa350235bddcfff4527aaed15b80
Edmond Atto <edmond091@gmail.com>
2020-07-02 23:58:11 -0400

    Add note on binary search solution

--- LeetCode/easy/sqrt.js
+++ LeetCode/easy/sqrt.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 function mySqrt(x) {
+  // A more optimal solution (O(log(n))) can be found using binary search
   for (let i = 0; i <= x; i++) {
     if (i * i === x) return i
     if (i * i > x) return i - 1
